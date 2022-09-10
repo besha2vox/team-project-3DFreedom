d561ad8f15b9abfeb2acb146d91cf33afd57d91c
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-09-10 09:32:19 +0300

    mobile done, but not all

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,7 +2,7 @@
 const mobileMenu = document.querySelector('.js-menu-container');
 const toggleMenuBtn = document.querySelector('.toggle-menu');
 const openMenuBtn = document.querySelector('.js-open-menu');
-const closeMenuBtn = document.querySelector('.js-close-menu');
+// const closeMenuBtn = document.querySelector('.js-close-menu');
 
 const toggleMenu = () => {
 const isMenuOpen =
@@ -13,11 +13,11 @@ toggleMenuBtn.classList.toggle('is-open');
 const scrollLockMethod = !isMenuOpen
     ? 'disableBodyScroll'
     : 'enableBodyScroll';
-bodyScrollLock[scrollLockMethod](document.body);
+// bodyScrollLock[scrollLockMethod](document.body);
 };
 
 openMenuBtn.addEventListener('click', toggleMenu);
-closeMenuBtn.addEventListener('click', toggleMenu);
+// closeMenuBtn.addEventListener('click', toggleMenu);
 
 // Закрываем мобильное меню на более широких экранах
 // в случае изменения ориентации устройства.
@@ -26,6 +26,6 @@ window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    // bodyScrollLock.enableBodyScroll(document.body);
 });
 })();
